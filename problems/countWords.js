e885d71af485649337ce683247fb38b03798c48b
Jordan Bobadilla <jordanboba08@gmail.com>
2021-02-03 17:47:06 -0400

    countWords done

--- problems/countWords.js
+++ problems/countWords.js
@@ -1,21 +1,23 @@
 /**
-* Takes an array of words and counts how many times each word occurs in the array.
-* 
-* @param {string[]} arr - An array of lowercase words
-* @returns {object} - an object where the key is the word, and the value is the count of that word
-*
-* ex: countWords(["good", "good", "great"]);
-* returns: { "good": 2, "great": 1 }
-* 
-* ex: countWords(["dog", "cat", "goat"]);
-* returns: { "dog": 1, "cat": 1, "goat": 1}
-* 
-* ex: countWords([]);
-* returns: {}
-*/
+ * Takes an array of words and counts how many times each word occurs in the array.
+ *
+ * @param {string[]} arr - An array of lowercase words
+ * @returns {object} - an object where the key is the word, and the value is the count of that word
+ *
+ * ex: countWords(["good", "good", "great"]);
+ * returns: { "good": 2, "great": 1 }
+ *
+ * ex: countWords(["dog", "cat", "goat"]);
+ * returns: { "dog": 1, "cat": 1, "goat": 1}
+ *
+ * ex: countWords([]);
+ * returns: {}
+ */
 
-function countWords() {
-  
-}
+const countWords = (arr) => {
+  let obj = {};
+  arr.forEach(el => obj[el] ? obj[el]++ : obj[el] = 1)
+  return obj
+};
 
-module.exports = countWords
+module.exports = countWords;
